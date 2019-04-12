1761360390230b4bb18294638a1aefa84cb3bef5
JudgeFudge <gabor.wermer@greenfoxacademy.com>
2019-04-12 11:49:55 +0200

    load

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,9 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { TodoService } from 'src/app/services/todo.service';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Todo } from 'src/app/models/Todo';
-import { HttpErrorResponse } from '@angular/common/http';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -11,7 +11,7 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
-  todos: Observable<Todo[]>;
+  todos$: Observable<Todo[]>;
   error: string;
 
   constructor(private todoSvc: TodoService) {}
@@ -21,7 +21,7 @@ export class TodosComponent implements OnInit {
   }
 
   loadTodos(): void {
-    this.todos = this.todoSvc.findAll().pipe(
+    this.todos$ = this.todoSvc.findAll().pipe(
       catchError((err: HttpErrorResponse) => {
         this.error = err.message;
         return of([]);
@@ -42,6 +42,16 @@ export class TodosComponent implements OnInit {
   }
 
   toggleError(): void {
-    this.error = this.error ? '' : 'Something went wrong!';
+    this.error = this.error ? null : 'Something went wrong!';
+    if (this.error) {
+      this.todos$ = of([]);
+    } else {
+      this.loadTodos();
+    }
+  }
+
+  toggleTodos(): void {
+    this.loadTodos();
+    this.error = null;
   }
 }
